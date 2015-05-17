ac803d049b02c7d2efa66156742bb299c35791fb
Beau Gunderson <beau@beaugunderson.com>
2015-05-17 00:24:59 -0700

    Initial commit

---
+++ fax-pipe.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var concat = require('concat-stream');
+var PhaxioAPI = require('phaxio-api');
+var streamifier = require('streamifier');
+
+var program = require('commander');
+
+program
+  .version('1.0.0')
+  .option('-p, --phone-number <number>', 'The phone number to send the fax to')
+  .option('-k, --key <key>', 'A Phaxio key')
+  .option('-s, --secret <secret>', 'A Phaxio secret')
+  .option('-c, --callback-url <url>', 'A Phaxio callback URL')
+  .parse(process.argv);
+
+if (!program.phoneNumber) {
+  console.error('Specify a phone number.');
+
+  process.exit(1);
+}
+
+var key = program.key || process.env.PHAXIO_KEY;
+var secret = program.secret || process.env.PHAXIO_SECRET;
+var callbackUrl = program.callbackUrl || process.env.PHAXIO_CALLBACK_URL;
+
+if (!key || !secret) {
+  console.error('Specify a key or secret.');
+
+  process.exit(1);
+}
+
+var phaxio = new PhaxioAPI({
+  api_key: key,
+  api_secret: secret,
+  callback_url: callbackUrl
+});
+
+process.stdin.pipe(concat(function (buffer) {
+  // TODO: use the non-stream API to avoid this silliness?
+  phaxio.send(program.phoneNumber, {
+    stream: streamifier.createReadStream(buffer),
+    filename: 'file.pdf',
+    knownLength: buffer.length
+  }).then(function (response) {
+    console.log('phaxio response', response);
+  }) .catch(function (err) {
+    console.log('phaxio err', err);
+  });
+}));
